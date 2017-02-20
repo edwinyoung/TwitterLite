9316efa71deb9ea409a55c5ef9ba633a3bfcc64f
Ed Young <edwin.w.young@gmail.com>
2017-02-19 22:12:12 -0800

    Initial commit

---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'TwitterLite' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for TwitterLite
+  pod 'AFNetworking'
+  pod 'BDBOAuth1Manager'
+
+end
---
+++ TwitterLite/Controllers/TwitterViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TwitterViewController.swift
+//  TwitterLite
+//
+//  Created by Edwin Young on 2/19/17.
+//  Copyright © 2017 Test Org Pls Ignore. All rights reserved.
+//
+
+import UIKit
+
+class TwitterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
